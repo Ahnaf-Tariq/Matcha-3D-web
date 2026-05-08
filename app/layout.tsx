19667b8dd9255937401ac821cdf3704cde10be0e
Ahnaf Tariq <ahnafhamid7@gmail.com>
2026-05-08 16:02:52 +0500

    feat: lenis scroll smooth

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { PropsWithChildren } from "react";
 import CustomCursor from "@/components/cursor/custom-cursor";
+import SmoothScroll from "@/components/lenis-scroll/smooth-scroll";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <SmoothScroll />
         <CustomCursor />
         {children}
       </body>
